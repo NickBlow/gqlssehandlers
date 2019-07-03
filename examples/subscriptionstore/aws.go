780b85ab2843fbb2414fda66d4136cfa9a39ba67
nick <nick@wakelet.com>
2019-07-03 19:53:07 +0100

    update subscription store to be a struct containing table name

--- examples/subscriptionstore/aws.go
+++ examples/subscriptionstore/aws.go
@@ -34,16 +34,21 @@ func getSession() *session.Session {
 	return sess
 }
 
+// DDBStore wraps a DynamoDB database
+type DDBStore struct {
+	TableName string
+}
+
 // StoreSubscriptionInDDB stores the subscription data in DDB with a TTL.
 // Table schema should be primary key ClientID with a sort key of SubscriptionID, and the field 'TTL' as the ttl
-func StoreSubscriptionInDDB(subscriberData subscriptions.Data, queryData subscriptions.Query, tableName string) error {
+func (d *DDBStore) StoreSubscriptionInDDB(subscriberData subscriptions.Data, queryData subscriptions.Query) error {
 	ttl := getSubscriptionTTL()
 	variables, err := dynamodbattribute.MarshalMap(queryData.VariableValues)
 	if err != nil {
 		return err
 	}
 	_, err = ddbSvc.PutItem(&dynamodb.PutItemInput{
-		TableName: aws.String(tableName),
+		TableName: aws.String(d.TableName),
 		Item: map[string]*dynamodb.AttributeValue{
 			"TTL": {
 				N: aws.String(strconv.FormatInt(ttl, 10)),
@@ -73,12 +78,13 @@ type FullSubscriptionData struct {
 
 // GetSubscriptionsFromDDB gets the subscription data from DDB - this is not required by the interface, but on reconnect of a client,
 // we should check that we're processing all their active subscriptions.
-func GetSubscriptionsFromDDB(ClientID string) []FullSubscriptionData {
+func (d *DDBStore) GetSubscriptionsFromDDB(ClientID string) []FullSubscriptionData {
 	return []FullSubscriptionData{}
 }
 
 // RemoveSubscriptionFromDDB removes the subscription data in DDB.
-func RemoveSubscriptionFromDDB(subscriberData subscriptions.Data, tableName string) error {
+func (d *DDBStore) RemoveSubscriptionFromDDB(subscriberData subscriptions.Data) error {
+
 	return nil
 }
 
